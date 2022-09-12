e30b142f1260d77eec38d3beadbbeedf522e59b9
Muhammad  Sagor <70064576+Sagor0078@users.noreply.github.com>
2022-09-12 22:54:42 +0600

    Update mergeSort05.py

--- mergeSort05.py
+++ mergeSort05.py
@@ -1,63 +1,29 @@
+def merge(left,right):
+  result = []
+  i,j = 0, 0 
+  while i < len(left) and j < len(right):
+      if left[i] <= right[j]:
+        result.append(left[i])
+        i+=1
+      else:
+        result.append(right[j])
+        j+=1
+  result += left[i:]
+  result += right[j:]
+  return result
+       
 
-#https://www.geeksforgeeks.org/merge-sort/
-# Python program for implementation of MergeSort
-def mergeSort(arr):
-	if len(arr) > 1:
-
-		# Finding the mid of the array
-		mid = len(arr)//2
-
-		# Dividing the array elements
-		L = arr[:mid]
-
-		# into 2 halves
-		R = arr[mid:]
-
-		# Sorting the first half
-		mergeSort(L)
-
-		# Sorting the second half
-		mergeSort(R)
-
-		i = j = k = 0
-
-		# Copy data to temp arrays L[] and R[]
-		while i < len(L) and j < len(R):
-			if L[i] < R[j]:
-				arr[k] = L[i]
-				i += 1
-			else:
-				arr[k] = R[j]
-				j += 1
-			k += 1
-
-		# Checking if any element was left
-		while i < len(L):
-			arr[k] = L[i]
-			i += 1
-			k += 1
-
-		while j < len(R):
-			arr[k] = R[j]
-			j += 1
-			k += 1
-
-# Code to print the list
+def mergesort(arr):
+    if (len(arr) <= 1):
+        return arr
+    mid = len(arr) // 2
+    left = mergesort(arr[:mid])
+    right = mergesort(arr[mid:])
+    return merge(left,right)
 
 
-def printList(arr):
-	for i in range(len(arr)):
-		print(arr[i], end=" ")
-	print()
 
-
-# Driver Code
-if __name__ == '__main__':
-	arr = [12, 11, 13, 5, 6, 7]
-	print("Given array is", end="\n")
-	printList(arr)
-	mergeSort(arr)
-	print("Sorted array is: ", end="\n")
-	printList(arr)
-
-# This code is contributed by Mayank Khanna
+# n = int(input())
+arr = [9,8,7,5,6,2,3,1]
+print('sorted array : ')
+print(mergesort(arr))

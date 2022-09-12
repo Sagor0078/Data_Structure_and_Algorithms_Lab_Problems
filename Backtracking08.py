a547cc4edde39b5bef2636fbc02e55698cb64324
Muhammad  Sagor <70064576+Sagor0078@users.noreply.github.com>
2022-09-12 23:34:12 +0600

    Update Backtracking08.py

--- Backtracking08.py
+++ Backtracking08.py
@@ -1,3 +1,16 @@
+
+# from itertools import permutations
+
+# N=4
+# sol=0
+# cols = range(N)
+# for combo in permutations(cols):                      
+#     if N==len(set(combo[i]+i for i in cols))==len(set(combo[i]-i for i in cols)):
+#         sol += 1
+#         print('Solution '+str(sol)+': '+str(combo)+'\n')  
+#         print("\n".join(' o ' * i + ' X ' + ' o ' * (N-i-1) for i in combo) + "\n\n\n\n")
+
+
 def is_attack(i,j,b,N):
     for k in range(1,i):
         if(b[k][j] == 1):

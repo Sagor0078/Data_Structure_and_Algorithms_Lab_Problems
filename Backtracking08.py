0a99ff7a117ea0a9a7926aee36adb398ae905358
Muhammad  Sagor <70064576+Sagor0078@users.noreply.github.com>
2022-09-12 22:25:44 +0600

    Update Backtracking08.py

--- Backtracking08.py
+++ Backtracking08.py
@@ -1,51 +1,36 @@
-# https://www.codesdope.com/course/algorithms-backtracking/
-
-
-def is_attack(i, j, board, N):
-  # checking for column j
-  for k in range(1, i):
-    if(board[k][j] == 1):
-      return True
-
-  # checking upper right diagonal
-  k = i-1
-  l = j+1
-  while (k>=1 and l<=N):
-    if (board[k][l] == 1):
-      return True
-    k=k+1
-    l=l+1
-
-  # checking upper left diagonal
-  k = i-1
-  l = j-1
-  while (k>=1 and l>=1):
-    if (board[k][l] == 1):
-      return True
-    k=k-1
-    l=l-1
-
-  return False
-
-def n_queen(row, n, N, board):
-  if (n==0):
-    return True
-
-  for j in range(1, N+1):
-    if(not(is_attack(row, j, board, N))):
-      board[row][j] = 1
-
-      if (n_queen(row+1, n-1, N, board)):
+def is_attack(i,j,b,N):
+    for k in range(1,i):
+        if(b[k][j] == 1):
+          return True
+    k = i-1
+    l = j+1
+    while(k>=1 and l<=N):
+      if(b[k][l] == 1):
         return True
+      k = k+1
+      l = l+1
+    k = i-1
+    l = j-1
+    while(k>=1 and l>=1):
+      if(b[k][l] == 1):
+        return True
+      k = k-1
+      l = l-1
+    return False
 
-      board[row][j] = 0 #backtracking
-  return False
+def n_queen(r,n,N,b):
+    if (n == 0):
+       return True
+    for j in range(1,N+1):
+        if(not(is_attack(r,j,b,N))):
+          b[r][j] = 1
+          if(n_queen(r+1,n-1,N,b)): # bug hocchilo
+            return True
+          b[r][j] = 0
+    return False
 
-if __name__ == '__main__':
-  board = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
 
-  n_queen(1, 4, 4, board)
-
-  #printing the matix
-  for i in range(1, 5):
-      print(board[i][1:])
+b = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+n_queen(1,4,4,b)
+for i in range(1,5):
+    print(b[i][1:])
